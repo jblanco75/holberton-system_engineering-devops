e9260006e160e7a100bb79038b3fb5a1fef2d1ec
jblanco75 <jblanco75@gmail.com>
2021-09-27 23:14:55 +0000

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    rqst = requests.get(url, headers={'User-agent': 'jb75'},
+                        allow_redirects=False).json()
+    if rqst.status_code == 200:
+        suscribers = r.json().get("data").get("subscribers")
+        return (suscribers)
+    return (0)
